1d85cc1c4b5fee2e07703183c3e4f9e5adea3b92
Olgu Örs Başak <olguors@gmail.com>
2023-04-09 21:18:51 +0300

    Created Box

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Rotating Box</h1>
+    <Perspective />
+  </div>
+</template>
+
+<script>
+import Perspective from "./components/Perspective.vue";
+
+export default {
+  components: {
+    Perspective,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Perspective.vue
@@ -0,0 +1,61 @@
+<!-- src/components/RotateBox.vue -->
+<template>
+  <div>
+    <div class="rotate-box" :style="rotateStyle"></div>
+    <div class="controls">
+      <p>Rotate X</p>
+      <Slider class="slider" v-model="x" :min="-180" :max="180" />
+      <p>Rotate Y</p>
+      <Slider class="slider" v-model="y" :min="-180" :max="180" />
+      <p>Rotate Z</p>
+      <Slider class="slider" v-model="z" :min="-180" :max="180" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Slider from "primevue/slider";
+
+export default {
+  components: {
+    Slider,
+  },
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+  },
+  computed: {
+    rotateStyle() {
+      return {
+        transform: `rotateX(${this.x}deg) rotateY(${this.y}deg) rotateZ(${this.z}deg)`,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.rotate-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 300px;
+  background-image: linear-gradient(to right, #437797, #2724dc);
+  border: 1px solid #2724dc;
+  margin: 0 auto;
+  transition: transform 0.5s;
+}
+
+.controls {
+  margin-top: 20px;
+}
+.slider {
+  width: 200px;
+  margin: 0 auto;
+  background-image: linear-gradient(to right, #437797, #2724dc);
+}
+</style>
